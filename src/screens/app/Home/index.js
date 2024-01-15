065b7f1407bba5fb8bdc9fdac06781ab7a3fa894
tinhph2 <tinhph2@fe.edu.vn>
2024-01-15 11:09:46 +0700

    add productlist

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -5,30 +5,43 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import Header from '../../../components/Header';
 import {categories} from '../../../components/data/categories';
 import CategoryBox from '../../../components/CategoryBox';
+import {products} from '../../../components/data/products';
+import ProductHomeItem from '../../../components/ProductHomeItem';
 const Home = () => {
   const [keyword, setKeyword] = useState(false);
   const renderCategoryItem = ({item, index}) => {
     return <CategoryBox title={item?.title} image={item?.image}></CategoryBox>;
   };
 
+  const renderProductItem = ({item}) => {
+    return <ProductHomeItem {...item} />;
+  };
+
   return (
     <SafeAreaView>
-      <ScrollView style={styles.container}>
-        <Header
-          showSearch
-          onSearch={setKeyword}
-          keyword={keyword}
-          title="Find All You Need"
-        />
-        <FlatList
-          showsHorizontalScrollIndicator={false}
-          style={styles.list}
-          horizontal
-          data={categories}
-          renderItem={renderCategoryItem}
-          keyExtractor={(item, index) => String(index)}
-        />
-      </ScrollView>
+      {/* <ScrollView style={styles.container}> */}
+      <Header
+        showSearch
+        onSearch={setKeyword}
+        keyword={keyword}
+        title="Find All You Need"
+      />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        style={styles.list}
+        horizontal
+        data={categories}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item, index) => String(index)}
+      />
+      <FlatList
+        style={styles.productsList}
+        numColumns={2}
+        data={products}
+        renderItem={renderProductItem}
+        keyExtractor={item => String(item.id)}
+      />
+      {/* </ScrollView> */}
     </SafeAreaView>
   );
 };
